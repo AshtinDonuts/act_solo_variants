774c952d2f2bc2b547976934bc720d9fc9051780
AshtinDonuts <kingwonghku@gmail.com>
2026-02-25 20:32:15 +1030

    save files

--- prepare_scp.sh
+++ prepare_scp.sh
@@ -1,4 +1,5 @@
-# Transfer 
+# Transfer from Local to Server
 scp -r /mnt/c2d9b23a-b03e-4fdb-82ad-59f039ec9e3e/khw/board_wipe 10.12.65.153:/home/khw/aloha_data/board_wipe
 
-scp -r /mnt/c2d9b23a-b03e-4fdb-82ad-59f039ec9e3e/khw/board_wipe 10.12.65.153:/home/khw/aloha_data/board_wipe
+# Transfer from Server to Local
+scp -v -r 10.12.65.153:/home/khw/act_solo_variants/act/ckpt_dir/board_wipe_chunk50 /home/khw/ACT_old/act/ckpt_dir
