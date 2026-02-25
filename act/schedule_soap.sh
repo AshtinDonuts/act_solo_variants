026d6425522bc4d8a493bb2480284a670527b977
AshtinDonuts <kingwonghku@gmail.com>
2026-02-25 15:58:19 +1030

    File save

--- act/schedule_soap.sh
+++ act/schedule_soap.sh
@@ -5,10 +5,11 @@
 ## REMEMBER TO ACTIVATE ENVIRONMENT ##
 
 # TRAIN 1
-# RQ1
+# Model: ACT Base
+# Cam Views: 3 views
 python3 imitate_episodes_record_torque.py \
---task_name lego_insert \
---ckpt_dir ./ckpt_dir/lego_insert \
+--task_name soap_push \
+--ckpt_dir ./ckpt_dir/soap_push \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -16,17 +17,18 @@ python3 imitate_episodes_record_torque.py \
 --hidden_dim 512 \
 --batch_size 16 \
 --dim_feedforward 3200 \
---num_epochs 6000 \
+--num_epochs 8000 \
 --lr 2e-5 --seed 0 \
-> lego_insert.log 2>&1
+> soap_push.log 2>&1
 
 sleep 30  # Increased cooldown to ensure file handles are closed and GPU is clear
 
-# LEGO-INSERT
 # TRAIN 2
+# Model: ACT Base - Obs joints as Target
+# Cam Views: 3 views
 python3 imitate_episodes_record_torque.py \
---task_name lego_insert \
---ckpt_dir ./ckpt_dir/lego_insert_obs_target \
+--task_name soap_push \
+--ckpt_dir ./ckpt_dir/soap_push_obs_target \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -36,50 +38,16 @@ python3 imitate_episodes_record_torque.py \
 --dim_feedforward 3200 \
 --num_epochs 8000 \
 --lr 2e-5 --seed 0 --use_obs_target \
-> lego_insert_obs_target.log 2>&1
-sleep 30
-
-#######################
-##### BOARD WIPE ######
-
-#1
-python3 imitate_episodes_record_torque.py \
---task_name board_wipe \
---ckpt_dir ./ckpt_dir/board_wipe \
---robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
---policy_class ACT \
---kl_weight 10 \
---chunk_size 100 \
---hidden_dim 512 \
---batch_size 16 \
---dim_feedforward 3200 \
---num_epochs 6000 \
---lr 2e-5 --seed 0 \
-> board_wipe.log 2>&1
+> soap_push_obs_target.log 2>&1
 
 sleep 30
 
-# 2
+# TRAIN 3
+# Model: ACT Base
+# Cam Views: 2 views (right shoulder, wrist)
 python3 imitate_episodes_record_torque.py \
---task_name board_wipe \
---ckpt_dir ./ckpt_dir/board_wipe_obs_target \
---robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
---policy_class ACT \
---kl_weight 10 \
---chunk_size 100 \
---hidden_dim 512 \
---batch_size 16 \
---dim_feedforward 3200 \
---num_epochs 8000 \
---lr 2e-5 --seed 0 --use_obs_target \
-> board_wipe_obs_target.log 2>&1
-
-sleep 30
-
-# 3
-python3 imitate_episodes_record_torque.py \
---task_name board_wipe \
---ckpt_dir ./ckpt_dir/board_wipe_2view \
+--task_name soap_push \
+--ckpt_dir ./ckpt_dir/soap_push_2view \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -90,14 +58,16 @@ python3 imitate_episodes_record_torque.py \
 --num_epochs 8000 \
 --exclude_cameras camera_left_shoulder \
 --lr 2e-5 --seed 0 \
-> board_wipe_2view.log 2>&1
+> soap_push_2view.log 2>&1
 
 sleep 30
 
-#4
+# TRAIN 4
+# Model: ACT Base - Obs joints as Target
+# Cam Views: 2 views (right shoulder, wrist)
 python3 imitate_episodes_record_torque.py \
---task_name board_wipe \
---ckpt_dir ./ckpt_dir/board_wipe_2view_obs_target \
+--task_name soap_push \
+--ckpt_dir ./ckpt_dir/soap_push_2view_obs_target \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -108,14 +78,16 @@ python3 imitate_episodes_record_torque.py \
 --num_epochs 8000 \
 --exclude_cameras camera_left_shoulder \
 --lr 2e-5 --seed 0 --use_obs_target \
-> board_wipe_2view_obs_target.log 2>&1
+> soap_push_2view_obs_target.log 2>&1
 
 sleep 30
 
-#5
+# TRAIN 5
+# Model: ACT Base
+# Cam Views: 1 view (wrist)
 python3 imitate_episodes_record_torque.py \
---task_name board_wipe \
---ckpt_dir ./ckpt_dir/board_wipe_1view \
+--task_name soap_push \
+--ckpt_dir ./ckpt_dir/soap_push_1view \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -123,17 +95,19 @@ python3 imitate_episodes_record_torque.py \
 --hidden_dim 512 \
 --batch_size 16 \
 --dim_feedforward 3200 \
---num_epochs 6000 \
+--num_epochs 8000 \
 --exclude_cameras camera_right_shoulder camera_left_shoulder \
 --lr 2e-5 --seed 0 \
-> board_wipe_1view.log 2>&1
+> soap_push_1view.log 2>&1
 
 sleep 30
 
-#6
+# TRAIN 6
+# Model: ACT Base - Obs joints as Target
+# Cam Views: 1 view (wrist)
 python3 imitate_episodes_record_torque.py \
---task_name board_wipe \
---ckpt_dir ./ckpt_dir/board_wipe_1view_obs_target \
+--task_name soap_push \
+--ckpt_dir ./ckpt_dir/soap_push_1view_obs_target \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -141,11 +115,10 @@ python3 imitate_episodes_record_torque.py \
 --hidden_dim 512 \
 --batch_size 16 \
 --dim_feedforward 3200 \
---num_epochs 6000 \
+--num_epochs 8000 \
 --exclude_cameras camera_right_shoulder camera_left_shoulder \
 --lr 2e-5 --seed 0 --use_obs_target \
-> board_wipe_1view_obs_target.log 2>&1
-
+> soap_push_1view_obs_target.log 2>&1
 
 ## TRAINING COMPLETED ##
 echo "Full training sequence complete at $(date)"
--- act/schedule_usb.sh
+++ act/schedule_usb.sh
@@ -8,8 +8,8 @@
 # Model: ACT Base
 # Cam Views: 3 views
 python3 imitate_episodes_record_torque.py \
---task_name lego_insert_h \
---ckpt_dir ./ckpt_dir/lego_insert_h \
+--task_name usb_plug \
+--ckpt_dir ./ckpt_dir/usb_plug \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -19,7 +19,7 @@ python3 imitate_episodes_record_torque.py \
 --dim_feedforward 3200 \
 --num_epochs 8000 \
 --lr 2e-5 --seed 0 \
-> lego_insert_h.log 2>&1
+> usb_plug.log 2>&1
 
 sleep 30  # Increased cooldown to ensure file handles are closed and GPU is clear
 
@@ -27,8 +27,8 @@ sleep 30  # Increased cooldown to ensure file handles are closed and GPU is clea
 # Model: ACT Base - Obs joints as Target
 # Cam Views: 3 views
 python3 imitate_episodes_record_torque.py \
---task_name lego_insert_h \
---ckpt_dir ./ckpt_dir/lego_insert_h_obs_target \
+--task_name usb_plug \
+--ckpt_dir ./ckpt_dir/usb_plug_obs_target \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -38,7 +38,7 @@ python3 imitate_episodes_record_torque.py \
 --dim_feedforward 3200 \
 --num_epochs 8000 \
 --lr 2e-5 --seed 0 --use_obs_target \
-> lego_insert_h_obs_target.log 2>&1
+> usb_plug_obs_target.log 2>&1
 
 sleep 30
 
@@ -46,8 +46,8 @@ sleep 30
 # Model: ACT Base
 # Cam Views: 2 views (right shoulder, wrist)
 python3 imitate_episodes_record_torque.py \
---task_name lego_insert_h \
---ckpt_dir ./ckpt_dir/lego_insert_h_2view \
+--task_name usb_plug \
+--ckpt_dir ./ckpt_dir/usb_plug_2view \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -58,7 +58,7 @@ python3 imitate_episodes_record_torque.py \
 --num_epochs 8000 \
 --exclude_cameras camera_left_shoulder \
 --lr 2e-5 --seed 0 \
-> lego_insert_h_2view.log 2>&1
+> usb_plug_2view.log 2>&1
 
 sleep 30
 
@@ -66,8 +66,8 @@ sleep 30
 # Model: ACT Base - Obs joints as Target
 # Cam Views: 2 views (right shoulder, wrist)
 python3 imitate_episodes_record_torque.py \
---task_name lego_insert_h \
---ckpt_dir ./ckpt_dir/lego_insert_h_2view_obs_target \
+--task_name usb_plug \
+--ckpt_dir ./ckpt_dir/usb_plug_2view_obs_target \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -78,7 +78,7 @@ python3 imitate_episodes_record_torque.py \
 --num_epochs 8000 \
 --exclude_cameras camera_left_shoulder \
 --lr 2e-5 --seed 0 --use_obs_target \
-> lego_insert_h_2view_obs_target.log 2>&1
+> usb_plug_2view_obs_target.log 2>&1
 
 sleep 30
 
@@ -86,8 +86,8 @@ sleep 30
 # Model: ACT Base
 # Cam Views: 1 view (wrist)
 python3 imitate_episodes_record_torque.py \
---task_name lego_insert_h \
---ckpt_dir ./ckpt_dir/lego_insert_h_1view \
+--task_name usb_plug \
+--ckpt_dir ./ckpt_dir/usb_plug_1view \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -95,10 +95,10 @@ python3 imitate_episodes_record_torque.py \
 --hidden_dim 512 \
 --batch_size 16 \
 --dim_feedforward 3200 \
---num_epochs 6000 \
+--num_epochs 8000 \
 --exclude_cameras camera_right_shoulder camera_left_shoulder \
 --lr 2e-5 --seed 0 \
-> lego_insert_h_1view.log 2>&1
+> usb_plug_1view.log 2>&1
 
 sleep 30
 
@@ -106,8 +106,8 @@ sleep 30
 # Model: ACT Base - Obs joints as Target
 # Cam Views: 1 view (wrist)
 python3 imitate_episodes_record_torque.py \
---task_name lego_insert_h \
---ckpt_dir ./ckpt_dir/lego_insert_h_1view_obs_target \
+--task_name usb_plug \
+--ckpt_dir ./ckpt_dir/usb_plug_1view_obs_target \
 --robot /home/khw/interbotix_ws/src/aloha/config/robot/aloha_solo \
 --policy_class ACT \
 --kl_weight 10 \
@@ -115,11 +115,10 @@ python3 imitate_episodes_record_torque.py \
 --hidden_dim 512 \
 --batch_size 16 \
 --dim_feedforward 3200 \
---num_epochs 6000 \
+--num_epochs 8000 \
 --exclude_cameras camera_right_shoulder camera_left_shoulder \
 --lr 2e-5 --seed 0 --use_obs_target \
-> lego_insert_h_1view_obs_target.log 2>&1
-
+> usb_plug_1view_obs_target.log 2>&1
 
 ## TRAINING COMPLETED ##
 echo "Full training sequence complete at $(date)"
